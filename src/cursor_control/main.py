6ac6deb4980adedf5e0c7291aece34133d399205
Jency Shijin <jshijin12@gmail.com>
2023-12-10 01:36:08 -0500

    code cleanup

--- src/cursor_control/main.py
+++ src/cursor_control/main.py
@@ -1,6 +1,6 @@
 from camera_module import initialize_camera, read_frame, flip_frame, release_camera
 from face_module import process_face, draw_landmarks, move_cursor
-from control_module import double_click, process_blink_action
+from control_module import double_click, process_blink_action,right_click, move_cursor_to
 import pyautogui
 import mediapipe as mp
 import cv2
@@ -29,12 +29,10 @@ def main():
         if count:
             if landmark_points:
                 landmarks = landmark_points[0].landmark
-                draw_landmarks(frame, landmarks, frame_w, frame_h)
-                x = int(landmarks[475].x * frame_w)
-                y = int(landmarks[475].y * frame_h)
+                screen_x, screen_y = draw_landmarks(frame, landmarks, frame_w, frame_h, screen_w, screen_h)
 
                 # Check if the cursor moves correctly
-                move_cursor(frame, x, y)
+                move_cursor_to(screen_x, screen_y)
 
                 left = [landmarks[153], landmarks[158]]
                 process_blink_action(frame, left, double_click, 0.006, frame_w, frame_h)
@@ -44,15 +42,33 @@ def main():
                 
                 # Processing Mouth landmarks
                 mouth = [landmarks[15], landmarks[13]]
+                for landmark in mouth:
+                    x = int(landmark.x * frame_w)
+
+                    y = int(landmark.y * frame_h)
+                cv2.circle(frame, (x, y), 3, (0, 255, 254))
                 if mouth[0].y - mouth[1].y > 0.08:
                     count = False
                     print("a")
                     pyautogui.sleep(0.2)
                     time.sleep(2)
-
         else:
+            if landmark_points:
+                landmarks = landmark_points[0].landmark
+                mouth = [landmarks[15], landmarks[13]]
+                for landmark in mouth:
+                    x = int(landmark.x * frame_w)
+
+                    y = int(landmark.y * frame_h)
+                    cv2.circle(frame, (x, y), 3, (0, 255, 255))
+                if (mouth[0].y - mouth[1].y) > 0.08:
+                    count = True
+                    print("b")
+                    pyautogui.sleep(0.2)
+                    time.sleep(1)
+
             # Handle the case when count is False
-            cv2.imshow('Eye Control cursor', frame)
+        cv2.imshow('Eye Control cursor', frame)
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
